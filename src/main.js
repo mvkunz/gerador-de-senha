a72ab09531e113eab6afe9c7d51df16ca34d8d05
Maria Vitória Kunz <mvkunz.dev@gmail.com>
2023-06-21 18:45:50 -0300

    finalizado

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { nanoid } from 'nanoid';
+import './style.css';
 
 const passwordBtnEl = document.querySelector('button');
 // console.log(passwordBtnEl); Esse console serve para conferir se, de fato, pegouo botão no html.
