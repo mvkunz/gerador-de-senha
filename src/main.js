b6ae60a55419370356b828ff7b41b1dcf7476c60
Maria Vitória Kunz <mvkunz.dev@gmail.com>
2023-06-21 20:09:19 -0300

    projeto finalizado

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { nanoid } from 'nanoid';
 import './style.css';
+import copy from 'clipboard-copy';
 
 const passwordBtnEl = document.querySelector('button');
 // console.log(passwordBtnEl); Esse console serve para conferir se, de fato, pegouo botão no html.
@@ -8,4 +9,9 @@ const displayPasswordEl = document.querySelector('h2');
 passwordBtnEl.addEventListener('click', () => {
   const randomPassword = nanoid();
   displayPasswordEl.innerHTML = randomPassword;
+});
+
+displayPasswordEl.addEventListener('click', () => {
+  copy(event.target.innerHTML);
+  alert('Senha copiada!');
 });
